06a4aab930b2fe85ffe2edbef2b8d236e1c5572f
unknown <divyacharaninabha@gmail.com>
2020-10-14 00:13:32 +0530

    Added : Welcome message added

---
+++ src/om/bridgelabz/hashtable/Main.java
@@ -0,0 +1,12 @@
+package om.bridgelabz.hashtable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Welcome Message
+		System.out.println("Welcome to hashtable program");
+
+	}
+
+}
